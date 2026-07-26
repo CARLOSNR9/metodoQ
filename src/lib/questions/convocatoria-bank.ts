4783368c5cce13dc8897fdddcc1b7fa0a4d32eed
CARLOS NARVAEZ <carlosnr9@gmail.com>
2026-07-26 18:04:30 -0500

    metodoq-jul26-12

--- src/lib/questions/convocatoria-bank.ts
+++ src/lib/questions/convocatoria-bank.ts
@@ -1,6 +1,7 @@
 import { UCC_CONV_2025_06_21_QUESTIONS } from "@/data/ucc-conv-2025-06-21-questions";
 import { UCC_CONV_2025_07_05_QUESTIONS } from "@/data/ucc-conv-2025-07-05-questions";
 import { UCC_CONV_2025_07_19_QUESTIONS } from "@/data/ucc-conv-2025-07-19-questions";
+import { UCC_CONV_2026_07_26_QUESTIONS } from "@/data/ucc-conv-2026-07-26-questions";
 import { enrichQuestionsWithTheoryPills } from "@/lib/questions/enrich-theory-pills";
 import type { TrainingQuestion } from "@/lib/questions/types";
 
@@ -8,6 +9,7 @@ const BANK_BY_EDITION: Record<string, TrainingQuestion[]> = {
   "UCC-2025-06-21": UCC_CONV_2025_06_21_QUESTIONS,
   "UCC-2025-07-05": UCC_CONV_2025_07_05_QUESTIONS,
   "UCC-2025-07-19": UCC_CONV_2025_07_19_QUESTIONS,
+  "UCC-2026-07-26": UCC_CONV_2026_07_26_QUESTIONS,
 };
 
 /** Preguntas de una edición de convocatoria (aisladas del entrenamiento libre). */
